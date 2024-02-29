34b9efdfefab4952dc06494a9c9b270f009742b4
TwinHter <nhdnguyen05@gmail.com>
2024-02-29 20:12:37 +0700

    Initialize

--- sinh.cpp
+++ sinh.cpp
@@ -1,9 +1,6 @@
 #include <bits/stdc++.h>
 using namespace std;
-// Tên chương trình
 const string NAME = "template";
-// Số test kiểm tra
-const int NTEST = 100;
 #define ll long long
 
 mt19937 rd(chrono::steady_clock::now().time_since_epoch().count());
@@ -24,13 +21,8 @@ int main()
     freopen("sinh.out", "w", stdout);
     srand(time(NULL));
     string lowcase = "abcdefghijklmnopqrstuvwxyz";
-    int T = 100;
-    cout << T << '\n';
     while(T--) {
-        ll n, x;
-        n = Rand(5e16, 1e17-1);
-        x = Rand(1, 1e5);
-        if(T % 20 == 0) x = n+1;
-        cout << n << " " << x << '\n';
+      int n = 1000000; cin >> n;
+      
     }
 }
