efd28c3b9909945929589275d87e5ca6949a860c
TwinHter <nhdnguyen05@gmail.com>
2024-03-03 00:09:00 +0700

    Initialize

---
+++ Quick_sort.cpp
@@ -0,0 +1,11 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+const int SZ = 1000000 + 10;
+int a[SZ];
+int main() {
+  freopen("test_case", "r", stdin);
+  int n; cin >> n;
+  for(int i=1; i<=n; i++) cin >> a[i];
+  cout << n << '\n';
+} 
